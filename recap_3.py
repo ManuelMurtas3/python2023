65f8d8f91d226b0fcfe95d50bc6c6eeb90d4add9
Manuel Murtas <manuel.murtas3@gmail.com>
2023-05-12 12:41:34 +0200

    recap 3 (commentato)

--- recap_3.py
+++ recap_3.py
@@ -5,9 +5,11 @@ lista = [1, 2, 2, 5, 1, 6, 6, 8, 4, 2, 1]
 lista_risultato = []
 numeri = set()
 
+#aggiungo tutti i numeri ad un set (struttura dati che non ammette duplicati)
 for numero in lista:
     numeri.add(numero)
 
+#mantenendo l'ordine della lista di input vado ad inserire i numeri nella lista risultato solo la prima volta che appaiono
 for numero in lista:
     if numero in numeri:
         lista_risultato.append(numero)
