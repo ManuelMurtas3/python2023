aa0d22c78994aecfb5594ee9937db2618592d5af
Manuel Murtas <manuel.murtas3@gmail.com>
2023-05-16 17:21:42 +0200

    aggiunto commento

--- settimana mirko/230516/dado.py
+++ settimana mirko/230516/dado.py
@@ -7,7 +7,7 @@ class Dado:
         facce = numero_facce
 
     def lancia(self):
-        return int(((random() * 100) % self.facce) + 1)
+        return int(((random() * 100) % self.facce) + 1) #il limite di facce è 100
 
 dado = Dado(20)
 print(dado.lancia())
