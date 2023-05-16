3b963b9b9f469774cc063da8f64671d30cc22865
Manuel Murtas <manuel.murtas3@gmail.com>
2023-05-16 17:20:33 +0200

    dado.py

---
+++ settimana mirko/230516/dado.py
@@ -0,0 +1,13 @@
+from random import random
+
+class Dado:
+    facce = 20
+
+    def __init__(self, numero_facce):
+        facce = numero_facce
+
+    def lancia(self):
+        return int(((random() * 100) % self.facce) + 1)
+
+dado = Dado(20)
+print(dado.lancia())
