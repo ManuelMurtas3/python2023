0407205e471a49de2a40ce9dbdcf311c600d0ecb
Manuel Murtas <manuel.murtas3@gmail.com>
2023-05-17 11:17:48 +0200

    Correzione esercizio 1

--- settimana mirko/230517/ereditarieta2.py
+++ settimana mirko/230517/ereditarieta2.py
@@ -7,21 +7,27 @@ class Uno:
         print(f"Numero = {self.numero}")
 
 class Due(Uno):
-    def __init__(self, numero):
+    def __init__(self, numero, decimale):
         super().__init__(numero)
+        self.decimale = decimale
     def to_string(self):
         super().to_string()
+        print(f"Decimale = {self.decimale}")
 
 class Tre(Due):
-    def __init__(self, numero):
-        super().__init__(numero)
+    def __init__(self, numero, decimale, stringa):
+        super().__init__(numero, decimale)
+        self.stringa = stringa
     def to_string(self):
         super().to_string()
+        print(f"Stringa = {self.stringa}")
 
 uno = Uno(1)
-due = Due(2)
-tre = Tre(3)
+due = Due(1, 2.0)
+tre = Tre(1, 2.0, "3")
 
 uno.to_string()
+print()
 due.to_string()
+print()
 tre.to_string()
