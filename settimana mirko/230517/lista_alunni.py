4b1a0276be401c7c94d9d2bbc8b2c835163ee413
Manuel Murtas <manuel.murtas3@gmail.com>
2023-05-17 12:36:18 +0200

    esercizio lista alunni

--- settimana mirko/230517/lista_alunni.py
+++ settimana mirko/230517/lista_alunni.py
@@ -2,3 +2,149 @@
 #Un primo menu con entra o esci, se esci si chiude tutto, se entri devi darmi X nomi obbligatori e X corrispettivi voti agli studenti
 # L'esercizio sarà creare un sistema che permetta di andare a scegliere un singolo utente e di modificare e aggiungere i suoi voti 
 
+class Alunno:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def to_string(self):
+        return f"Alunno: {self.nome}"
+
+class SchedaAlunno(Alunno):
+
+    def __init__(self, nome, voto):
+        super().__init__(nome)
+        self.voti = list()
+        self.voti.append(voto)
+
+    def modifica_voto(self, indice, voto):
+        if 0 <= indice < len(self.voti):
+            self.voti[indice] = voto
+
+    def aggiungi_voto(self, voto):
+        self.voti.append(voto)
+
+    def to_string(self):
+        return super().to_string() + f" | Voti: {' '.join([str(voto) for voto in self.voti])}"
+    
+class ListaAlunni:
+
+    lista_schede_alunni = []
+
+    def __init__(self, nomi, voti):
+        for i in range(len(nomi)):
+            self.lista_schede_alunni.append(SchedaAlunno(nomi[i], voti[i]))
+
+    def cerca_alunno(self, nome):
+        for alunno in self.lista_schede_alunni:
+            if alunno.nome == nome:
+                return alunno
+        return None
+    
+    def stampa_registro(self):
+        for scheda in self.lista_schede_alunni:
+            print(scheda.to_string())
+
+def switch_accesso():
+    option = "ok"
+    while option != "0":
+        print("Benvenuto! Selezione l'opzione desiderata:")
+        print("1. Accedi")
+        print("0. Esci")
+        option = input("Inserisci la tua scelta: ")
+        print()
+
+        if option == "1":
+            #accesso al sistema lista alunni
+            alunni = input("Inserisci la lista dei nomi degli alunni: ").strip().lower().split(" ")
+            voti = input("Inserisci la lista di voti degli alunni: ").strip().lower().split(" ")
+            print()
+            lista_alunni = ListaAlunni(alunni, [int(voto) for voto in voti])
+            switch_registro(lista_alunni)
+        elif option == "0":
+            #uscita dal programma
+            print("Arrivederci")
+            break
+        else:
+            print("Errore, l'opzione da te selezionata non esiste\n")
+            continue
+
+def switch_registro(lista_alunni):
+    option = "ok"
+    while option != "0":
+        print("Selezione l'opzione desiderata:")
+        print("1. Visualizza registro")
+        print("2. Gestisci voti alunno")
+        print("0. Esci")
+        option = input("Inserisci la tua scelta: ")
+        print()
+
+        if option == "1":
+            #visualizza registro
+            lista_alunni.stampa_registro()
+            print()
+
+        elif option == "2":
+            #gestisci voti alunno
+            nome = input("Inserisci il nome dell'alunno di cui gestire i voti: ")
+            print()
+            alunno = lista_alunni.cerca_alunno(nome)
+            switch_alunno(alunno)
+
+        elif option == "0":
+            #uscita dal programma
+            break
+        else:
+            print("Errore, l'opzione da te selezionata non esiste\n")
+            continue
+
+def switch_alunno(alunno):    
+    option = "ok"
+    while option != "0":
+        print("Selezione l'opzione desiderata:")
+        print("1. Modifica voto")
+        print("2. Aggiungi voto")
+        print("0. Esci")
+        option = input("Inserisci la tua scelta: ")
+        print()
+
+        if option == "1":
+            #modifica voto
+            indice = switch_voto(alunno)
+            nuovo_voto = int(input("Inserisci il nuovo voto: "))
+            alunno.modifica_voto(indice, nuovo_voto)
+            print("Voto modificato correttamente\n")
+
+        elif option == "2":
+            #aggiungi voto
+            voto = int(input("Inserisci un nuovo voto: "))
+            alunno.aggiungi_voto(voto)
+            print("Voto aggiunto correttamente\n")
+
+        elif option == "0":
+            #uscita dal programma
+            break
+        else:
+            print("Errore, l'opzione da te selezionata non esiste\n")
+            continue
+
+def switch_voto(alunno):
+    exit_flag = False
+    while not exit_flag:
+        print("Seleziona il voto da modificare: ")
+        numero_opzione = 1
+        for voto in alunno.voti:
+            print(f"{numero_opzione}. {voto}")
+            numero_opzione += 1
+
+        option = int(input("Inserisci il numero del dell'indice del voto: "))
+        print()
+
+        #selezione del piatto in base all'indice nel menu
+        if option >= 1 and option <= len(alunno.voti):
+            return option - 1
+        else:
+            print("Errore, l'opzione da te selezionata non esiste\n")
+            continue
+
+switch_accesso()
